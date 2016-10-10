e7b17ce201e8b0773ab1bf517be86b93360b8791
Minijackson <minijackson@riseup.net>
2016-10-10 13:20:09 +0200

    Put main loop in main fn

--- src/display.rs
+++ src/display.rs
@@ -1,36 +1,43 @@
 extern crate piston_window;
 extern crate image;
+extern crate gfx_device_gl;
 
-use self::piston_window::{PistonWindow, WindowSettings, clear, Texture, TextureSettings};
-use self::image::{ImageBuffer, Rgba, ConvertBuffer};
+use self::piston_window::{
+    PistonWindow,
+    clear,
+    Texture,
+    TextureSettings,
+    GenericEvent
+};
 
-use std::sync::mpsc::Receiver;
+use self::gfx_device_gl::Resources;
 
-pub fn listener(receiver : Receiver<ImageBuffer<Rgba<u8>, Vec<u8>>>) {
-    let mut window : PistonWindow =
-        WindowSettings::new("Piston: image", [300, 300])
-        .exit_on_esc(true)
-        .build()
-        .unwrap();
+use self::image::{
+    ImageBuffer,
+    Rgba
+};
 
-    let mut tex : Option<Texture<_>> = None;
+pub fn build_texture(frame  : ImageBuffer<Rgba<u8>, Vec<u8>>,
+                     tex    : Option<Texture<Resources>>,
+                     window : &mut PistonWindow)
+        -> Option<Texture<Resources>> {
 
-    while let Some(e) = window.next() {
-        if let Ok(frame) = receiver.try_recv() {
-
-            if let Some(mut t) = tex {
-                t.update(&mut window.encoder, &frame).unwrap();
-                tex = Some(t);
-            } else {
-                tex = Texture::from_image(&mut window.factory, &frame, &TextureSettings::new()).ok();
-            }
-        }
-
-        window.draw_2d(&e, |c, g| {
-            clear([1.0; 4], g);
-            if let Some(ref t) = tex {
-                piston_window::image(t, c.transform, g);
-            }
-        });
+    if let Some(mut t) = tex {
+        t.update(&mut window.encoder, &frame).unwrap();
+        return Some(t);
+    } else {
+        return Texture::from_image(&mut window.factory, &frame, &TextureSettings::new()).ok();
     }
+}
+
+pub fn show_texture<E>(texture : &Option<Texture<Resources>>,
+                       window  : &mut PistonWindow,
+                       event   : E) 
+        where E : GenericEvent {
+    window.draw_2d(&event, |context, graphics| {
+        clear([1.0; 4], graphics);
+        if let Some(ref t) = *texture {
+            piston_window::image(t, context.transform, graphics);
+        }
+    });
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
+extern crate piston_window;
+
 use std::sync::mpsc::channel;
 
 mod capture;
 mod display;
 
+use self::piston_window::{
+    PistonWindow,
+    WindowSettings,
+    Texture
+};
+
 fn main() {
+
+    let mut window : PistonWindow =
+        WindowSettings::new("Piston: image", [300, 300])
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut displayed_texture : Option<Texture<_>> = None;
+
     let (sender, receiver) = channel();
 
     capture::stream(sender);
-    display::listener(receiver);
+
+    while let Some(e) = window.next() {
+        if let Ok(frame) = receiver.try_recv() {
+            displayed_texture = display::build_texture(frame, displayed_texture, &mut window);
+        }
+
+        display::show_texture(&displayed_texture, &mut window, e);
+    }
 }
